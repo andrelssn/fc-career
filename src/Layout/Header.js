66c41d54e2a812da5335de2bf814b87cd7e90dd7
andrelssn <andreluissilsan@hotmail.com>
2025-05-03 17:45:00 -0300

    layout - feat: header idea

---
+++ src/Layout/Header.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { AppBar, Box, Tab, Tabs, Typography } from "@mui/material";
+
+// img
+import img from "../Images/Logo/futebol (1).png";
+
+// Styles
+import "./Style.css";
+
+export default function Header() {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <AppBar className="header-style">
+            <Box display={"inline-flex"} justifyContent={"right"} alignItems={"center"}>
+                <Box position={"absolute"} left={30}>
+                    <Typography variant="h5" display={"flex"} alignItems={"center"}>
+                        <img src={img} style={{ width: 40, height: 40, marginRight: 10 }}/>
+                        <span>FC Career Generator</span>
+                    </Typography>
+                </Box>
+
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    sx={{
+                        '& .MuiTabs-indicator': {
+                            backgroundColor: 'var(--orange)', // Cor do indicador
+                        },
+                        '& .MuiTab-root': {
+                            color: 'var(--text-secondary)', // Cor do texto das abas
+                            fontWeight: 'bold',
+                        },
+                        '& .Mui-selected': {
+                            color: 'var(--orange)', // Cor da aba selecionada
+                        },
+                    }}
+                >
+                    <Tab label="Página Inicial" />
+                    <Tab label="Gerador" />
+                    <Tab label="Informações" />
+                </Tabs>
+            </Box>
+        </AppBar>
+    );
+}
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -2,11 +2,14 @@ import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 
 // Components
 import MainRouter from "../Router/MainRouter";
+import Header from "./Header";
 
 export default function Layout() {
 
     return (
         <main>
+            <Header/>
+
             <MainRouter/>
         </main>
     );
